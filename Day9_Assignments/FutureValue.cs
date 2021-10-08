075fca4f1a0e4e3ef109cfc7b6d4082033e145a5
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-09 02:19:49 +0530

    [Ankit]Add : Check Prime Number , Calculates Factorial ,Calculates Future Value

---
+++ Day9_Assignments/FutureValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_Assignments
+{
+    class FutureValue
+    {
+        public static void Value()
+        {
+            Console.WriteLine("Enter the amount of money in Dollars you will invest");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the time in years you will invest money");
+            double t = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the rate at which you will invest money");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double futureValue = c * Math.Pow((1 + r) , t );
+            Console.WriteLine("Your Future Value will be :"+futureValue);
+
+        }
+    }
+}
